83dea5baf9521800f1d311da03a046601954c02b
Claudio <inglese.claudio@yahoo.it>
2025-04-18 16:57:17 +0200

    Aggiornamento Front-end 1.4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,37 +1,39 @@
 <x-layout>
-    <div class="sfondoServizi vh-100">
-        <div class="container-fluid">
-            <div class="row justify-content-center vw-100">
-                <div class="col-12 text-center mt-5">
-                    <h1 class="primario marginTop">Login</h1>
-                    <div class="text-center">
-                        <p class="primario">Non hai ancora un account? <a class="text-decoration-none terziario" href="{{ route('register') }}">Registrati!</a></p>
-                    </div>
+    <div class="container-fluid sfondoServizi vh-100">
+        
+        <div class="row justify-content-center">
+            <div class="col-12 text-center mt-5">
+                <h1 class="primario marginTop">Login</h1>
+                <div class="text-center">
+                    <p class="primario">Non hai ancora un account? <a class="text-decoration-none terziario" href="{{ route('register') }}">Registrati!</a></p>
                 </div>
             </div>
         </div>
+        
         <div class="row justify-content-center">
             <div class="col-12 col-md-6">
                 <form method="POST" action="{{ route('login')}} ">
                     @csrf
-                <div class="mb-3">
-                    <label for="email" class="form-label primario">Indirizzo Email</label>
-                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" aria-describedby="emailHelp" name="email" value="{{ old('email') }}">
-                    @error('email')
+                    <div class="mb-3">
+                        <label for="email" class="form-label primario">Indirizzo Email</label>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" aria-describedby="emailHelp" name="email" value="{{ old('email') }}">
+                        @error('email')
                         <div class="text-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label primario">Inserisci Password</label>
-                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
-                    @error('password')
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label primario">Inserisci Password</label>
+                        <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
+                        @error('password')
                         <div class="text-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <a class="text-decoration-none secondario" href="{{ route('password.request') }}">Hai dimenticato la password?</a>
-                </div>
-                <button type="submit" class="btn btn-primary">Accedi</button>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <a class="text-decoration-none secondario" href="{{ route('password.request') }}">Hai dimenticato la password?</a>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Accedi</button>
                 </form>
+            </div>    
         </div>
-    </x-layout>
+    </div>    
+</x-layout>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="container-fluid navbar navbar-expand-lg coloreNavbar position-fixed">
   <div class="row justify-content-center align-items-center">
     <div class="col-12">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand ms-3" href="#">
         <img src="../img/Icona_PS.png" class="iconaNavbar" alt="logo pomosoftware">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,12 +16,11 @@
           <a class="nav-link vociNavbar coloreNavTitle" aria-current="page" href="{{ route('homepage') }}">HOME</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link vociNavbar annunciNavbar coloreNavTitle" aria-current="page" href="#">ANNUNCI</a>
+          <a class="nav-link annunciNavbar coloreNavTitle" aria-current="page" href="{{route('article.index')}}">ANNUNCI</a>
         </li>
         @guest
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle vociNavbar coloreNavTitle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Guest
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item vociNavbar coloreNavTitle" href="{{ route('register') }}">Registrati</a></li>
@@ -31,14 +30,15 @@
         @endguest
         @auth
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle vociNavbar coloreNavTitle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             {{ Auth::user()->name }}
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item vociNavbar" href="#">Action</a></li>
-            <li><a class="dropdown-item vociNavbar" href="#">Another action</a></li>
+            <li><a href="{{route('create.article')}}" class="dropdown-item vociNavbar coloreNavTitle">Crea</a></li>
+            <li><a class="dropdown-item vociNavbar coloreNavTitle" href="#">Action</a></li>
+            <li><a class="dropdown-item vociNavbar coloreNavTitle" href="#">Another action</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item vociNavbar" href="#" onclick="event.preventDefault(); document.querySelector('#logout').submit()">Logout</a></li>
+            <li><a class="dropdown-item vociNavbar coloreNavTitle" href="#" onclick="event.preventDefault(); document.querySelector('#logout').submit()">Logout</a></li>
             <form method="POST" action="{{ route('logout') }}" id="logout">
               @csrf
             </form>
@@ -48,7 +48,7 @@
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-dark vociNavbar" type="submit">Cerca</button>
+        <button class="btn vociNavbar coloreNavTitle me-3" type="submit">Cerca</button>
       </form>
     </div>
   </div>
