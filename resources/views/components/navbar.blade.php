0d3ca260bb603c8aa893dc075f8b4c42e10a6c33
Gianluca Michelini <gianlucamichelini@gmail.com>
2025-04-18 18:18:09 +0200

    Commit obbligato per pullare ultimo aggiornamento front Cla

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -21,6 +21,7 @@
         @guest
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle vociNavbar coloreNavTitle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          GUEST
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item vociNavbar coloreNavTitle" href="{{ route('register') }}">Registrati</a></li>
