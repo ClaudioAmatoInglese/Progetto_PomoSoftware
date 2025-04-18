3d7ccb526d3ab97c3bc9e9cb61e4deefb99f9267
NiccoloLuciani <niccolo.luciani@outlook.it>
2025-04-18 11:41:05 +0200

    6 articoli nella home e index articoli

---
+++ resources/views/components/article-card.blade.php
@@ -0,0 +1,11 @@
+<div class="card mx-auto card-w text-center">
+    <img src="" alt="placeholder{{$article->title}}" class="card-img-top">
+    <div class="card-body">
+        <h4 class="card-title">{{$article->title}}</h4>
+        <h6 class="card-subtitle">{{$article->price}}</h6>
+        <div>
+            <a href="" class="btn btn-primary">dettaglio annuncio</a>
+            <a href="" class="btn btn-secondary">categoria</a>
+        </div>
+    </div>
+</div>
