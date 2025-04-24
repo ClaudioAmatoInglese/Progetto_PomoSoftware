4b37987b0012cb99827c15f8556256dafc1c376b
Gianluca Michelini <gianlucamichelini@gmail.com>
2025-04-24 10:14:54 +0200

    User Story 3 completata

--- resources/views/mail/become-revisor.blade.php
+++ resources/views/mail/become-revisor.blade.php
@@ -15,7 +15,7 @@
         <p>Nome: {{ $user->name }}</p>
         <p>Email: {{ $user->email }}</p>
         <p>Se vuoi renderl* revisor, clicca qui:</p>
-        <a href="#">Rendi revisore</a>
+        <a href="{{ route('make.revisor', compact('user')) }}">Rendi revisore</a>
     </div>
 </body>
 
