c186b9d86eba25f180e02931fe1a9ff0e1835c24
NiccoloLuciani <niccolo.luciani@outlook.it>
2025-04-25 13:42:57 +0200

    modifica al footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,106 +1,117 @@
 <!-- Footer -->
 <footer class="text-center">
-    <!-- Grid container -->
-    <div class="container-fluid text-center justify-content-center align-items-center coloreFooter pb-5">
-      <!-- Section: Form -->
-      <section class="row text-center justify-content-center align-items-center pt-5">
-          <!--Grid row-->
-          <div class="row d-flex justify-content-around">
-            <!--Grid column-->
-            <div class="col-md-5 col-12">
-              <!-- Email input -->
-              <div data-mdb-input-init class="mb-4">
-                <form action="{{ route('contact.store') }}" method="POST">
-                  @csrf
-                  <p class="pt-2 primario textShadow3">
-                    <strong>Vuoi lavorare con noi? Inviaci il tuo contatto email:</strong>
-                  </p>
-                  <input type="email" id="form5Example24" class="form-control" name="email" required>
-                  <button data-mdb-ripple-init type="submit" class="btn sfondoBottone2 vociNavbar bordoScritte2 bordoBottone coloreNavTitle mt-2">
-                    Invia
-                  </button>        
-                </form>
-              </div>
-            </div>
-            <div class="col-md-5 col-12">
-              <h5 class="primario textShadow3 sottotitolo">Vuoi diventare nostro revisore?</h5>
-              <p class="primario textShadow3 sottotitolo">Cliccando il pulsante sottostante farai richiesta al nostro Admin!</p>
-              <a href="{{ route('become.revisor') }}" class="btn sfondoBottone2 vociNavbar bordoScritte2 bordoBottone coloreNavTitle mt-2">Diventa revisore</a>
-            </div>
-            <!--Grid column-->
+  <!-- Grid container -->
+  <div class="container-fluid text-center justify-content-center align-items-center coloreFooter pb-5">
+    <!-- Section: Form -->
+    <section class="row text-center justify-content-center align-items-center pt-5">
+      <!--Grid row-->
+      <div class="row d-flex justify-content-around">
+        <!--Grid column-->
+        <div class="col-md-5 col-12">
+          <!-- Email input -->
+          <div data-mdb-input-init class="mb-4">
+            <form action="{{ route('contact.store') }}" method="POST">
+              @csrf
+              <p class="pt-2 primario textShadow3">
+                <strong>Vuoi lavorare con noi? Inviaci il tuo contatto email:</strong>
+              </p>
+              <input type="email" id="form5Example24" class="form-control" name="email" required>
+              <button data-mdb-ripple-init type="submit" class="btn sfondoBottone2 vociNavbar bordoScritte2 bordoBottone coloreNavTitle mt-2">
+                Invia
+              </button>        
+            </form>
           </div>
-          <!--Grid row-->
-      </section>
-      <!-- Section: Form -->
-  
-      <!-- Section: Text -->
-      <section class="mb-4">
-        
-      </section>
-      <!-- Section: Text -->
-  
-      <!-- Section: Links -->
-      <section class="">
-        <!--Grid row-->
-        <div class="row">
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-            <h5 class="text-uppercase terziario bordoScritte2">G. Michelini</h5>
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a class="noDecor primario textShadow3" href="#!">JAVA Oriented</a>
-              </li>
-            </ul>
-          </div>
-          <!--Grid column-->
-  
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-            <h5 class="text-uppercase terziario bordoScritte2">N. Luciani</h5>
-  
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a class="noDecor primario textShadow3" href="#!">JAVA Oriented</a>
-              </li>
-            </ul>
-          </div>
-          <!--Grid column-->
-  
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-            <h5 class="text-uppercase terziario bordoScritte2">F. P. Guccione</h5>
-  
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a class="noDecor primario textShadow3" href="#!">ReactJS Oriented</a>
-              </li>
-            </ul>
-          </div>
-          <!--Grid column-->
-  
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-            <h5 class="text-uppercase terziario bordoScritte2">C. A. Inglese</h5>
-  
-            <ul class="list-unstyled mb-0 ">
-              <li>
-                <a class="primario noDecor textShadow3" href="#!">ReactJS Oriented</a>
-              </li>
-            </ul>
-          </div>
-          <!--Grid column-->
         </div>
-        <!--Grid row-->
-      </section>
-      <!-- Section: Links -->
-    </div>
-    <!-- Grid container -->
+        @auth
+        @if(Auth::user()->is_revisor == 0)
+        <div class="col-md-5 col-12">
+          <h5 class="primario textShadow3 sottotitolo">Vuoi diventare nostro revisore?</h5>
+          <p class="primario textShadow3 sottotitolo">Cliccando il pulsante sottostante farai richiesta al nostro Admin!</p>
+          <a href="{{ route('become.revisor') }}" class="btn sfondoBottone2 vociNavbar bordoScritte2 bordoBottone coloreNavTitle mt-2">Diventa revisore</a>
+        </div>
+        @endif
+        @endauth
+        @guest
+        <div class="col-md-5 col-12">
+          <h5 class="primario textShadow3 sottotitolo">Vuoi diventare nostro revisore?</h5>
+          <p class="primario textShadow3 sottotitolo">Per fare richiesta, effettua prima l'accesso al tuo account.</p>
+          <a href="{{route('login')}}" class="btn sfondoBottone2 vociNavbar bordoScritte2 bordoBottone coloreNavTitle mt-2">Accedi</a>
+        </div>
+        @endguest
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Form -->
+    
+    <!-- Section: Text -->
+    <section class="mb-4">
+      
+    </section>
+    <!-- Section: Text -->
+    
+    <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase terziario bordoScritte2">G. Michelini</h5>
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a class="noDecor primario textShadow3" href="#!">JAVA Oriented</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+        
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase terziario bordoScritte2">N. Luciani</h5>
+          
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a class="noDecor primario textShadow3" href="#!">JAVA Oriented</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+        
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase terziario bordoScritte2">F. P. Guccione</h5>
+          
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a class="noDecor primario textShadow3" href="#!">ReactJS Oriented</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+        
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase terziario bordoScritte2">C. A. Inglese</h5>
+          
+          <ul class="list-unstyled mb-0 ">
+            <li>
+              <a class="primario noDecor textShadow3" href="#!">ReactJS Oriented</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+  </div>
+  <!-- Grid container -->
   
-    <!-- Copyright -->
-    <div class="text-center p-3 linearGradient">
-      © 2025 Copyright:
-      <a class="extra bordoScritte2" href="https://mdbootstrap.com/">Pomo-Software.com</a>
-    </div>
-    <!-- Copyright -->
-  </footer>
-  <!-- Footer -->
+  <!-- Copyright -->
+  <div class="text-center p-3 linearGradient">
+    © 2025 Copyright:
+    <a class="extra bordoScritte2" href="https://mdbootstrap.com/">Pomo-Software.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
