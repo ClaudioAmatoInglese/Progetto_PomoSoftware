c75c634e9f58b1e26024f02404d08fef6ab663fc
Gianluca Michelini <gianlucamichelini@gmail.com>
2025-04-28 13:03:52 +0200

    AGGIORNAMENTO DELLA LOGICA DI SALVATAGGIO - da fare

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -4,13 +4,20 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
 use Illuminate\Support\Facades\Auth;
+
 
 class CreateArticleForm extends Component
 
 {
-
+    use WithFileUploads;
+    
+    public $images = [];
+    public $temporary_images;
+    
+    
     #[Validate('required|string|min:5')] 
     public $title;
     #[Validate('required|string|min:10|max:300')] 
@@ -20,38 +27,38 @@ class CreateArticleForm extends Component
     #[Validate('required')] 
     public $category;
     public $article;
-
+    
     public $categories;
-
+    
     public function mount(){
         
         $this->categories = Category::all();
-
+        
     }
-
-
-
-
+    
+    
+    
+    
     public function store(){
-
+        
         $this->validate();
-
-
+        
+        
         $article = Auth::user()->articles()->create([
             'title'=>$this->title,
             'description'=>$this->description,
             'price'=>$this->price,
             'category_id'=>$this->category,
         ]);
-
+        
         session()->flash('success', 'Hai creato il tuo Articolo Correttamente');
         
         $this->reset();
         
         return redirect(route('homepage'));
-
+        
     }
-
+    
     public function messages()
     {
         return [
@@ -65,15 +72,39 @@ class CreateArticleForm extends Component
             'price.min' => 'il prezzo deve essere maggiore di 0',
             'price.max' => 'il prezzo non può essere superiore a 99999999',
             'category.required' => 'Devi selezionare una categoria.',
-
+            
         ];
     }
-
     
-
-    public function render()
+    
+    public function updatedTemporaryImages()
     {
-        return view('livewire.create-article-form');
+        if ($this->validate([
+            'temporary_images.*' => 'image|max:1024',
+            'temporary_images' => 'max:6'
+            ])) {
+                foreach ($this->temporary_images as $image) {
+                    $this->images[] = $image;
+                }
+            }
+        }
+        
+        
+        public function removeImage($key)
+        {
+            if(in_array($key, array_keys($this->images))) {
+                
+                unset($this->images[$key]);
+                
+            }
+        }
+        
+        
+        
+        public function render()
+        {
+            return view('livewire.create-article-form');
+        }
+        
     }
-
-}
+    
