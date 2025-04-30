88fbacf588962463de4da38b7ab51b86efb85e80
Francesco <frenk0004@gmail.com>
2025-04-30 11:52:32 +0200

    impostazione user story 9

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -3,8 +3,7 @@
 namespace App\Jobs;
 
 use Spatie\Image\Image;
-
-
+// use Spatie\Image\Enums\Unit;
 use Spatie\Image\Enums\CropPosition;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -39,4 +38,24 @@ class ResizeImage implements ShouldQueue
             ->crop($w, $h, CropPosition::Center)
             ->save($destPath);
     }
+
+    // public function handle(): void
+    // {
+    //     $w = $this->w;
+    //     $h = $this->h;
+    //     $srcPath = storage_path() . '/app/public/' . $this->path . '/' . $this->fileName;
+    //     $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
+
+    //     Image::load($srcPath)
+    //         ->crop($w, $h, CropPosition::Center)
+    //         ->watermark(
+    //             base_path('public/img/watermark.png'),
+    //             width: 50,
+    //             height: 50,
+    //             paddingX: 5,
+    //             paddingY: 5,
+    //             paddingUnit: Unit::Percent
+    //         )
+    //         ->save($destPath);
+    // }
 }
